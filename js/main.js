744dc485fbef337f41a819925c984e3ace157ba9
Houben, Ron <ron.houben85@gmail.com>
2019-05-08 16:17:40 +0700

    Refactored the code

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ let videoElement
 let controls
 let sphere
 
-const ENABLE_AR = true
+const ENABLE_AR = false
 
 initialize()
 animate()
@@ -19,20 +19,13 @@ function initialize() {
     raycaster = new THREE.Raycaster()
     mouse = new THREE.Vector2()
     scene = new THREE.Scene()
+    scene.name = 'scene'
+    // create a markerRoot group
+    markerRoot = new THREE.Group()
+    markerRoot.name = 'markerRoot'
+    scene.add(markerRoot)
 
-    // camera = new THREE.PerspectiveCamera()
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000)
-    // Remove controls when ENABLE_AR = true?
-    if (ENABLE_AR === false) {
-        camera.position.set(0, 0, 1)
-        camera.position.y = Math.PI / 1
-        controls = new THREE.OrbitControls(camera)
-        controls.update()
-    }
-
-
-    scene.add(camera)
-
+    // setup the renderer and add it to the page
     renderer = new THREE.WebGLRenderer({
         antialias: true,
         alpha: true
@@ -41,81 +34,71 @@ function initialize() {
     renderer.setSize(WINDOW_WIDTH, WINDOW_HEIGHT)
     document.body.appendChild(renderer.domElement)
 
+    // setup the camera and add it to the scene
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000)
+    scene.add(camera)
+
+    // setup the clock
     clock = new THREE.Clock()
     deltaTime = 0
     totalTime = 0
 
-    ////////////////////////////////////////////////////////////
-    // setup arToolkitSource
-    ////////////////////////////////////////////////////////////
+    // add eventlistener for window resizing & click/touch events
+    window.addEventListener('resize', () => onResize())
+    window.addEventListener('click', onClick, false)
+    window.addEventListener('touchend', onTouch, false)
+
     if (ENABLE_AR === true) {
+        /************************
+         * setup arToolkitSource
+         ***********************/
+        // create the arToolkitSource (webcam, img)
         arToolkitSource = new THREEx.ArToolkitSource({
             sourceType: 'webcam',
         })
-    }
+        // initiate the arToolkitSource
+        arToolkitSource.init(() => onResize())
 
-    function onResize() {
-        if (ENABLE_AR === true) {
-            arToolkitSource.onResizeElement()
-            arToolkitSource.copyElementSizeTo(renderer.domElement)
-            if (arToolkitContext.arController !== null) {
-                arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas)
-            }
-        }
-    }
-    // initiate webcam
-    if (ENABLE_AR === true) {
-        arToolkitSource.init(function onReady() {
-            onResize()
-        })
-    }
-
-    // handle resize event
-    window.addEventListener('resize', function() {
-        onResize()
-    })
-
-    ////////////////////////////////////////////////////////////
-    // setup arToolkitContext
-    ////////////////////////////////////////////////////////////
-    if (ENABLE_AR === true) {
-        // create atToolkitContext
+        /*************************
+         * setup arToolkitContext
+         ************************/
+        // create arToolkitContext
         arToolkitContext = new THREEx.ArToolkitContext({
             cameraParametersUrl: '../data/camera_para.dat',
             detectionMode: 'mono'
         })
-
-        // copy projection matrix to camera when initialization complete
-        arToolkitContext.init(function onCompleted() {
+        // copy project matrix to camera when initialization is complete
+        arToolkitContext.init(() =>
             camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix())
-        })
-    }
+        )
 
-    ////////////////////////////////////////////////////////////
-    // setup markerRoots
-    ////////////////////////////////////////////////////////////
-    // build markerControls
-    markerRoot = new THREE.Group()
-    scene.add(markerRoot)
-    if (ENABLE_AR === true) {
-        let markerControls = new THREEx.ArMarkerControls(arToolkitContext, markerRoot, {
+        /********************
+         * setup markerRoots
+         *******************/
+        // create ArMarkerControls
+        new THREEx.ArMarkerControls(arToolkitContext, markerRoot, {
             type: 'pattern', patternUrl: "../data/hiro.patt",
         })
+    } else if (ENABLE_AR === false) {
+        camera.position.set(0, 0, 1)
+        camera.position.y = Math.PI / 1
+        controls = new THREE.OrbitControls(camera)
+        controls.update()
     }
 
     ////////////////////////////////////////////////////////////
     // setup scene
     ////////////////////////////////////////////////////////////
 
-    let loader = new THREE.GLTFLoader()
-
     let sceneGroup = new THREE.Group()
-    markerRoot.add(sceneGroup)
-
-    function onProgress(xhr) { console.log((xhr.loaded / xhr.total * 100) + '% loaded') }
-    function onError(xhr) { console.log('An error happened') }
+    scene.getObjectByName('markerRoot').add(sceneGroup)
 
     // Load a glTF resource
+    let loader = new THREE.GLTFLoader()
+
+    function onProgress(gltfModel) { console.log((gltfModel.loaded / gltfModel.total * 100) + '% loaded') }
+    function onError() { console.log('An error happened') }
+
     loader.load(
         '../models/accenture-ar.gltf',
         function(group) {
@@ -151,14 +134,6 @@ function initialize() {
     let ambientLight = new THREE.AmbientLight(0x6F6666, 2)
     sceneGroup.add(ambientLight)
 
-    // SPHERE
-    let sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32)
-    let sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 })
-    sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
-    sphere.name = 'sphere'
-    sphere.position.set(1, -1, 0)
-    sceneGroup.add(sphere)
-
 
     // VIDEO
     let videoGroup = new THREE.Group()
@@ -175,13 +150,29 @@ function initialize() {
 
     // Make video mesh
     let videoMesh = new THREE.Mesh(
-        new THREE.CircleGeometry(0.01, 35),
+        new THREE.PlaneGeometry(0.1, 0.1),
         new THREE.MeshBasicMaterial({ map: videoTexture, side: THREE.DoubleSide })
     )
     videoMesh.rotation.x = -Math.PI / 2
     videoMesh.name = 'videoMesh'
 
     videoGroup.add(videoMesh)
+}
+
+// Resize logic
+function onResize() {
+    if (ENABLE_AR === true) {
+        arToolkitSource.onResizeElement()
+        arToolkitSource.copyElementSizeTo(renderer.domElement)
+        if (arToolkitContext.arController !== null) {
+            arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas)
+        }
+    } else {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+
+        renderer.setSize(window.innerWidth, window.innerHeight)
+    }
 }
 
 // update logic
@@ -199,17 +190,15 @@ function render() {
 
 // run game loop (update, render, repeat)
 function animate() {
-    window.addEventListener('click', onClick, false)
-    window.addEventListener('touchend', onTouch, false)
 
-    let x = scene.children[ 1 ].children[ 0 ].children[ 4 ].scale.x
-    let y = scene.children[ 1 ].children[ 0 ].children[ 4 ].scale.y
-    let z = scene.children[ 1 ].children[ 0 ].children[ 4 ].scale.z
+    let x = scene.getObjectByName('video').scale.x
+    let y = scene.getObjectByName('video').scale.y
+    let z = scene.getObjectByName('video').scale.z
     // Animate scaling of video
     if (x < 150 && z < 150) {
         x += 0.1
         z += 0.1
-        scene.children[ 1 ].children[ 0 ].children[ 4 ].scale.set(x, y, z)
+        scene.getObjectByName('video').scale.set(x, y, z)
     }
 
     requestAnimationFrame(animate)
@@ -223,33 +212,30 @@ function animate() {
 function onClick(event) {
     // calculate mouse position in normalized device coordinates
     // (-1 to +1) for both components
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1
 
     // update the picking ray with the camera and mouse position
-    raycaster.setFromCamera(mouse, camera);
+    raycaster.setFromCamera(mouse, camera)
     // calculate objects intersecting the picking ray
-    let intersects = raycaster.intersectObjects(scene.children, true);
+    let intersects = raycaster.intersectObjects(scene.children, true)
     intersects.forEach(intersect => intersect.object.name == 'videoMesh' ? playPauseVideo(videoElement) : null)
-    intersects.forEach(intersect => intersect.object.name == 'sphere' ? intersect.object.material.color.set(0xaff999) : null)
 }
 
 function onTouch(event) {
-    event.preventDefault()
-    mouse.x = (event.changedTouches[ 0 ].clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.changedTouches[ 0 ].clientY / window.innerHeight) * 2 + 1;
+    mouse.x = (event.changedTouches[ 0 ].clientX / window.innerWidth) * 2 - 1
+    mouse.y = -(event.changedTouches[ 0 ].clientY / window.innerHeight) * 2 + 1
 
     // update the picking ray with the camera and mouse position
-    raycaster.setFromCamera(mouse, camera);
+    raycaster.setFromCamera(mouse, camera)
     // calculate objects intersecting the picking ray
-    let intersects = raycaster.intersectObjects(scene.children, true);
+    let intersects = raycaster.intersectObjects(scene.children, true)
     intersects.forEach(intersect => intersect.object.name == 'videoMesh' ? playPauseVideo(videoElement) : null)
-    intersects.forEach(intersect => intersect.object.name == 'sphere' ? intersect.object.material.color.set(0xaff999) : null)
 }
 
 function playPauseVideo(video) {
     if (video.paused)
-        video.play();
+        video.play()
     else
-        video.pause();
+        video.pause()
 }
