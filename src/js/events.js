b594aec4060b3aaf5dd4063e0d9996857fa76223
Houben, Ron <ron.houben85@gmail.com>
2019-05-14 18:33:03 +0700

    fixed the onTouch and onClick events in events.js

--- src/js/events.js
+++ src/js/events.js
@@ -1,11 +1,11 @@
-import { getState } from './store'
+import { getState, setState } from './store'
 import { video } from './video';
 
 export const events = {
     addEventListeners: objectsClickActions => {
         window.addEventListener('resize', events.onResize)
         window.addEventListener('click', event => events.onClick({ event, objectsClickActions }), false)
-        window.addEventListener('touchend', events.onTouch, false)
+        window.addEventListener('touchend', event => events.onTouchend({ event, objectsClickActions }), false)
     },
     onResize: () => {
         const { enable_ar, camera, renderer, arToolkitSource } = getState()
@@ -26,40 +26,52 @@ export const events = {
         }
     },
     onClick: ({ event, objectsClickActions }) => {
-        const { raycaster, mouse, scene, camera } = getState()
+        const { mouse } = getState()
         // calculate mouse position in normalized device coordinates
         // (-1 to +1) for both components
         mouse.x = (event.clientX / window.innerWidth) * 2 - 1
         mouse.y = - (event.clientY / window.innerHeight) * 2 + 1
-
-        // update the picking ray with the camera and mouse position
-        raycaster.setFromCamera(mouse, camera)
-        // calculate objects intersecting the picking ray
-        const intersects = raycaster.intersectObjects(scene.children, true)
-        // handle the clicked objects
-        const closestIntersect = intersects[ 0 ]
-        // if there's a intersect, loop over the objectsClickActions
-        if (closestIntersect) {
-            objectsClickActions.forEach((object) => {
-                // Check if the objects name equals the intersects name.
-                if (object.objectName === closestIntersect.object.name) {
-                    // execute the objects action. Use null as paramater when htmlElement is omitted.
-                    object.action(object.htmlElement || null)
-                }
-            })
-        }
+        // set the updated mouse object in the global state store
+        setState({ mouse })
+        // exectute object action:
+        events.executeObjectAction(objectsClickActions)
     },
-    onTouch: (event) => {
-        const { raycaster, mouse, scene, camera, videoElement } = getState()
+    onTouchend: ({ event, objectsClickActions }) => {
+        const { mouse } = getState()
 
         mouse.x = (event.changedTouches[ 0 ].clientX / window.innerWidth) * 2 - 1
         mouse.y = -(event.changedTouches[ 0 ].clientY / window.innerHeight) * 2 + 1
-
+        // set the updated mouse object in the global state store
+        setState({ mouse })
+        // exectute object action:
+        events.executeObjectAction(objectsClickActions)
+    },
+    executeObjectAction: objectsClickActions => {
+        const { raycaster, mouse, scene, camera } = getState()
         // update the picking ray with the camera and mouse position
         raycaster.setFromCamera(mouse, camera)
         // calculate objects intersecting the picking ray
         const intersects = raycaster.intersectObjects(scene.children, true)
+        // get closed intersect
+        const closestIntersect = intersects[ 0 ]
+        // if there's a intersect, loop over the objectsClickActions
+        if (closestIntersect) {
+            objectsClickActions.forEach(object => {
+                // Check if the objects name equals the intersects name.
+                if (object.objectName === closestIntersect.object.name) {
+                    // execute the objects action. Use null as paramater when htmlElement is omitted.
+                    try {
+                        object.action(object.htmlElement || null)
+                    }
+                    catch (error) {
+                        const suggestionMsg = () => console.error('\nMake sure you have an action defined for the following object in objectsClickActions:', object)
+                        const subjectMsg = () => console.error('\nThis happened on the following object in objectsClickActions', object)
 
-        intersects.forEach(intersect => intersect.object.name == 'videoMesh' ? playPauseVideo(videoElement) : null)
+                        console.error(error)
+                        !object.action ? suggestionMsg() : subjectMsg()
+                    }
+                }
+            })
+        }
     }
 }
