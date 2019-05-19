00b4262546ce8ff5d260326aefdb000ff4a9cbe2
Houben, Ron <ron.houben85@gmail.com>
2019-05-19 16:52:12 +0700

    Functiones are scoped

--- src/js/events.js
+++ src/js/events.js
@@ -1,17 +1,15 @@
 import { typeChecker } from './typeChecker';
 
-export const events = function(
-    {
-        objectsClickActions,
-        enable_ar,
-        camera,
-        renderer,
-        arToolkitSource,
-        raycaster,
-        mouse,
-        scene
-    }
-) {
+export const events = function({
+    objectsClickActions,
+    enable_ar,
+    camera,
+    renderer,
+    arToolkitSource,
+    raycaster,
+    mouse,
+    scene
+}) {
     // Typechecking of local state
     objectsClickActions = typeChecker('array', [], 'objectsClickActions')(objectsClickActions)
     enable_ar = typeChecker('boolean', null, 'enable_ar')(enable_ar)
@@ -24,70 +22,72 @@ export const events = function(
     // Because the arToolkitSource will not be created if AR isn't enabled.
     enable_ar ? arToolkitSource = typeChecker('object', {}, 'arToolkitSource')(arToolkitSource) : null
 
+    function onResize() {
+        if (enable_ar) {
+            // call the arToolkitSource.onResizeElement() function.
+            arToolkitSource.onResizeElement()
+            // copy the renderer.domElement size to the arToolkitSource
+            // so the element is placed on top of the marker.
+            arToolkitSource.copyElementSizeTo(renderer.domElement)
+        } else {
+            // change the aspect of the camera to the new window size.
+            camera.aspect = window.innerWidth / window.innerHeight
+            // update the camera's ProjectionMatrix
+            camera.updateProjectionMatrix()
+            // set the size of the renderer to the new window size
+            renderer.setSize(window.innerWidth, window.innerHeight)
+        }
+    }
+    function onClick() {
+        // calculate mouse position in normalized device coordinates
+        // (-1 to +1) for both components
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+        mouse.y = - (event.clientY / window.innerHeight) * 2 + 1
+        // exectute object action:
+        executeObjectAction()
+    }
+    function onTouchend() {
+        // calculate mouse position in normalized device coordinates
+        // (-1 to +1) for both components
+        mouse.x = (event.changedTouches[ 0 ].clientX / window.innerWidth) * 2 - 1
+        mouse.y = -(event.changedTouches[ 0 ].clientY / window.innerHeight) * 2 + 1
+        // exectute object action:
+        executeObjectAction()
+    }
+    function executeObjectAction() {
+        // update the picking ray with the camera and mouse position
+        raycaster.setFromCamera(mouse, camera)
+        // calculate objects intersecting the picking ray
+        const intersects = raycaster.intersectObjects(scene.children, true)
+        // get closed intersect
+        const closestIntersect = intersects[ 0 ]
+        // if there's a intersect, loop over the objectsClickActions
+        if (closestIntersect) {
+            objectsClickActions.forEach(object => {
+                // Check if the objects name equals the intersects name.
+                if (object.objectName === closestIntersect.object.name) {
+                    // execute the objects action. Use null as paramater when htmlElement is omitted.
+                    try {
+                        object.action(object.htmlElement || null)
+                    }
+                    catch (error) {
+                        const suggestionMsg = () => console.error('\nMake sure you have an action defined for the following object in objectsClickActions:', object)
+                        const subjectMsg = () => console.error('\nThis happened on the following object in objectsClickActions', object)
+
+                        console.error(error)
+                        !object.action ? suggestionMsg() : subjectMsg()
+                    }
+                }
+            })
+        }
+    }
+
     return {
         addEventListeners() {
-            window.addEventListener('resize', this.onResize)
-            window.addEventListener('click', event => this.onClick(event))
-            window.addEventListener('touchend', event => this.onTouchend(event))
-        },
-        onResize() {
-            if (enable_ar) {
-                // call the arToolkitSource.onResizeElement() function.
-                arToolkitSource.onResizeElement()
-                // copy the renderer.domElement size to the arToolkitSource
-                // so the element is placed on top of the marker.
-                arToolkitSource.copyElementSizeTo(renderer.domElement)
-            } else {
-                // change the aspect of the camera to the new window size.
-                camera.aspect = window.innerWidth / window.innerHeight
-                // update the camera's ProjectionMatrix
-                camera.updateProjectionMatrix()
-                // set the size of the renderer to the new window size
-                renderer.setSize(window.innerWidth, window.innerHeight)
-            }
-        },
-        onClick() {
-            // calculate mouse position in normalized device coordinates
-            // (-1 to +1) for both components
-            mouse.x = (event.clientX / window.innerWidth) * 2 - 1
-            mouse.y = - (event.clientY / window.innerHeight) * 2 + 1
-            // exectute object action:
-            this.executeObjectAction()
-        },
-        onTouchend() {
-            // calculate mouse position in normalized device coordinates
-            // (-1 to +1) for both components
-            mouse.x = (event.changedTouches[ 0 ].clientX / window.innerWidth) * 2 - 1
-            mouse.y = -(event.changedTouches[ 0 ].clientY / window.innerHeight) * 2 + 1
-            // exectute object action:
-            this.executeObjectAction()
-        },
-        executeObjectAction() {
-            // update the picking ray with the camera and mouse position
-            raycaster.setFromCamera(mouse, camera)
-            // calculate objects intersecting the picking ray
-            const intersects = raycaster.intersectObjects(scene.children, true)
-            // get closed intersect
-            const closestIntersect = intersects[ 0 ]
-            // if there's a intersect, loop over the objectsClickActions
-            if (closestIntersect) {
-                objectsClickActions.forEach(object => {
-                    // Check if the objects name equals the intersects name.
-                    if (object.objectName === closestIntersect.object.name) {
-                        // execute the objects action. Use null as paramater when htmlElement is omitted.
-                        try {
-                            object.action(object.htmlElement || null)
-                        }
-                        catch (error) {
-                            const suggestionMsg = () => console.error('\nMake sure you have an action defined for the following object in objectsClickActions:', object)
-                            const subjectMsg = () => console.error('\nThis happened on the following object in objectsClickActions', object)
-
-                            console.error(error)
-                            !object.action ? suggestionMsg() : subjectMsg()
-                        }
-                    }
-                })
-            }
+            //execute addEventListeners
+            window.addEventListener('resize', onResize)
+            window.addEventListener('click', event => onClick(event))
+            window.addEventListener('touchend', event => onTouchend(event))
         }
     }
 }
--- src/js/loadingManager.js
+++ src/js/loadingManager.js
@@ -6,57 +6,61 @@ import { typeChecker } from './typeChecker';
 
 const validLoaders = [ 'GLTF', 'MTLL', 'OBJ', 'STL' ]
 
-export const loadingManager = {
-    loadObject: async ({ url, loaderType }) => {
+export function loadingManager() {
+    async function loadObject({ url, loaderType }) {
         url = typeChecker('string', '', 'url')(url)
         loaderType = typeChecker('string', '', 'loaderType')(loaderType)
         // Create new object loader
-        const loader = loadingManager.createNewLoader(loaderType)
+        const loader = createNewLoader(loaderType)
         // load the object
         const object = await loader
             .load(url)
-            .catch(loader.loaderOnError)
+            .catch(loaderOnError)
         // return the object
         return object
-    },
-    createNewLoader: loaderName => {
+    }
+
+    function createNewLoader(loaderName) {
         loaderName = typeChecker('string', '', 'loaderName')(loaderName)
-        const { loaderOnProgress } = loadingManager
 
         switch (typeof loaderName === 'string' ? loaderName.toLowerCase() : typeof loaderName) {
             case 'gltf':
-                return loadingManager.promisifyLoader(new GLTFLoader(), loaderOnProgress)
+                return promisifyLoader(new GLTFLoader(), loaderOnProgress)
             case 'mtl':
-                return loadingManager.promisifyLoader(new MTLLoader(), loaderOnProgress)
+                return promisifyLoader(new MTLLoader(), loaderOnProgress)
             case 'obj':
-                return loadingManager.promisifyLoader(new OBJLoader(), loaderOnProgress)
+                return promisifyLoader(new OBJLoader(), loaderOnProgress)
             case 'stl':
-                return loadingManager.promisifyLoader(new STLLoader(), loaderOnProgress)
+                return promisifyLoader(new STLLoader(), loaderOnProgress)
             case 'object':
-                return loadingManager.promisifyLoader(new loaderName(), loaderOnProgress)
+                return promisifyLoader(new loaderName(), loaderOnProgress)
             default:
                 return console.error(`You've choosen an invalid loader!\n Please choose one of the following loaders:\n${ validLoaders.toString() }`)
         }
-    },
-    promisifyLoader: (classicLoader, onProgress) => {
+    }
+
+    function promisifyLoader(classicLoader, onProgress) {
         classicLoader = typeChecker('object', {}, 'classicLoader')(classicLoader)
         onProgress = typeChecker('function', () => null, 'onProgress')(onProgress)
         //TODO: Change promiseLoader to async/await
         const promiseLoader = url =>
             new Promise((resolve, reject) =>
-                classicLoader.load(url, resolve, onProgress, reject)).catch(loadingManager.loaderOnError)
+                classicLoader.load(url, resolve, onProgress, reject)).catch(loaderOnError)
 
         return {
             originalLoader: classicLoader,
             load: promiseLoader,
         }
-    },
-    loaderOnProgress: object => {
+    }
+    function loaderOnProgress(object) {
         object = typeChecker('object', {}, 'object')(object)
         console.info(`Object ${ object.loaded / object.total * 100 }% loaded`)
-    },
-    loaderOnError: error => {
+    }
+
+    function loaderOnError(error) {
         error = typeChecker('object', {}, 'error')(error)
         console.error('An error happened with loading the object:\n', error)
     }
+
+    return { loadObject: loadObject() }
 }
